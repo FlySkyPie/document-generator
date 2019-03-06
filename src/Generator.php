e1a64eacd3dc7b2bb97248e2ed982fe44bf6ae0a
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2019-03-06 17:00:08 +0800

    change structure of generator.

--- src/Generator.php
+++ src/Generator.php
@@ -7,9 +7,36 @@ use FlySkyPie\DocumentGenerator\OdtRenderer;
 abstract class Generator {
 
   private $OdtRenderer;
+  private $ContentIndexs = [];
+  private $Parameter = [];
 
-  public function __construct($FilePath) {
+  public function __construct($FilePath, $Parameter) {
+    $this->Parameter = $Parameter;
+    $this->checkParameter();
     $this->OdtRenderer = new OdtRenderer($FilePath);
+    $this->loadContent();
+  }
+
+  private function checkParameter() {
+    foreach ($this->ContentIndexs as $ContentIndex) {
+      if (!array_key_exists($ContentIndex, $this->Parameter)) {
+        $this->Parameter["$ContentIndex"] = ""; //default is empty string
+      }
+    }
+  }
+
+  private function loadContent() {
+    //override
+  }
+
+  /*
+   * Rplace Key with Content from template.
+   * @param string $Key
+   * @param string $Content  
+   */
+
+  private function setContent($Key, $Content) {
+    $this->OdtRenderer->setVars($Key, $Content);
   }
 
   /*
@@ -28,10 +55,6 @@ abstract class Generator {
 
   public function exportAsAttachedFileexportAsAttachedFile($FileName = "") {
     $this->OdtRenderer->exportAsAttachedFile($FileName);
-  }
-
-  public function getRenderer() {
-    return $this->OdtRenderer;
   }
 
 }
